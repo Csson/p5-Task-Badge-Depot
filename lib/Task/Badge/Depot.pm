1333d69e5aa0e09b709e73ca9f5386954cb21c0b
Erik Carlsson <info@code301.com>
2015-03-14 22:04:15 +0100

    Init

---
+++ lib/Task/Badge/Depot.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Task::Badge::Depot;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Task::Badge::Depot;
+
+=head1 DESCRIPTION
+
+Task::Badge::Depot is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+use Task::Badge::Depot;
+
+fail('this test is TODO!');
+done_testing;
